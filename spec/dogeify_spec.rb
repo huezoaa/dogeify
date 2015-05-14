3d4528404050c3fcacee3f9dbb36ab6958c6666e
Angel Huezo <angelohuezo@gmail.com>
2015-05-14 09:01:06 -0400

    modified the dogeify_spec.rb file

--- spec/dogeify_spec.rb
+++ spec/dogeify_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 describe Dogeify do
-  it 'has a version number' do
-    expect(Dogeify::VERSION).not_to be nil
-  end
+  subject {Dogeify.new}
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#process' do
+    let(:input) {'My grandmom gave me a sweater for Christmas'}
+    let(:output){ subject.process(input)}
+
+    it 'converts to lowercase' do
+      expect(output.downcase).to eq output
+    end
+
+    it 'combines nouns with doge adjective' do
+      expect(output).to match /so grandmom./i
+      expect(output).to match /such seater./i
+      expect(output).to match /very christmas./id
+    end
+
+    it 'always appends "wow"' do
+      expect(output).to end_with 'wow'
+    end
+
+
   end
 end
